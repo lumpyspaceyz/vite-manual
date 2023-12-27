007f79a71453e2e7dba8eaa4f34c5e176789d125
lumpyspaceyz <yzever99@gmail.com>
2023-12-27 17:19:04 +0900

    [learn] vite-manual

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import '/src/styles/main.css'
+import { button } from "@/styles/main.module.css";
+import jsLogo from "@/assets/javascript.svg";
+import img from "@/assets/8b.jpg";
+
+
+const app = document.querySelector('#app');
+
+const template = /* html */`
+  <figure>
+    <img src="${img}" alt="" />
+    <figcaption>자바스크립트 로고</figcaption>
+  </figure>
+`
+
+app.insertAdjacentHTML('beforeend',template)
